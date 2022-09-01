58386cdb7f483d4f2447bc32b85466beca97db5d
Abdul Basit <abhiawan1227@gmail.com>
2022-09-01 14:46:54 +0500

    minor additions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello world');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+//enviroment variable
+dotenv.config();
+
+//for data in json format
+app.use(express.json());
+
+//Database Connection
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('DB Connected successfully'))
+    .catch(err => console.log(err));
+
+
+//Server Connection
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Server Connected Successfully on port ' + process.env.PORT);
+});
